c19bd2344ce9626221c1cfc96d45cf9f3da25411
Oluwatobi Bello <bellotobiloba01@gmail.com>
2026-02-11 10:58:31 +0100

    updated

--- after/plugin/gitblame-config.lua
+++ after/plugin/gitblame-config.lua
@@ -1,10 +1,14 @@
 local blame = require 'thrillee.git-blame'
 
+local group = vim.api.nvim_create_augroup('GitLensBlame', { clear = true })
+
 vim.api.nvim_create_autocmd('CursorHold', {
+  group = group,
   callback = blame.blameVirtText,
 })
 
 vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+  group = group,
   callback = blame.clearBlameVirtText,
 })
 
